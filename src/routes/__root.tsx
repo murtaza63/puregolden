861b2b9e9c310772d6f58adff1036a8784af7a2a
murtaza63 <murtaza63@gmail.com>
2026-09-26 08:37:10 +0400

    fix: remove Grok PWA branding

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -39,8 +39,7 @@ export const Route = createRootRoute({
     links: [
       { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
 
-      { rel: "manifest", href: "/__grok/manifest.webmanifest" },
-      { rel: "apple-touch-icon", href: "/__grok/icon-180.png" },
+
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
